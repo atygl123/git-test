9c28d173d64ffdb1251366c850c384f2157b9a79
ygl <2641355637@qq.com>
2023-01-12 22:37:13 +0800

    third commit

--- src/main/java/com/ygl/git/GitTest.java
+++ src/main/java/com/ygl/git/GitTest.java
@@ -3,6 +3,7 @@ package com.ygl.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
-        System.out.println("hello github");
+        System.out.println("hello github1");
+        System.out.println("hello github2");
     }
 }
